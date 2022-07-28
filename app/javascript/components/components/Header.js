a6225a8d59b1ff57da9b8dc0d7ba8bfd2285d96a
Don <101531305+don-yoon@users.noreply.github.com>
2022-07-28 10:18:11 -0700

    basic navigation completed

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BulletPoint from './BulletPoint'
 
 export default function Header() {
   return (
     <>
      <header className="bg-black">
-      <div className="inline-flex">
-        <BulletPoint/>
-        <h1 className="text-white text-3xl font-bold" style={{margin:"auto"}}> Don's Portfolio </h1>
-      </div>
+      <a href="/" >
+        <div className="inline-flex">
+            <BulletPoint/>
+            <h1 className="text-white text-3xl font-bold" style={{margin:"auto"}}> Don's Portfolio </h1>
+        </div>
+      </a>
     </header>
     </>
    
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BulletPoint from '../components/BulletPoint'
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="flex flex-col">
+        <div className="inline-flex">
+          <BulletPoint/>
+          <Link to="/ml5videoclass"><h1 className="text-3xl font-bold" style={{margin:"auto"}}> ml5 Video Classifier </h1></Link>
+        </div>
+        <div className="inline-flex">
+          <BulletPoint/>
+          <Link to="/ml5objecttrack"><h1 className="text-3xl font-bold" style={{margin:"auto"}}> ml5 Object Detection </h1></Link>
+        </div>
+        <div className="inline-flex">
+          <BulletPoint/>
+          <Link to="/reactthreefiber"><h1 className="text-3xl font-bold" style={{margin:"auto"}}> React Three Fiber </h1></Link>
+        </div>
+      </div>
     </>
   )
 }
